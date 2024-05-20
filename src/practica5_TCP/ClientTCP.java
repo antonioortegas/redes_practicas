e1824f4425296997b19554219a7a2a3b30477723
antonioortegas <antonioortega@uma.es>
2024-05-21 00:51:41 +0200

    copy boilerplate from assignment

---
+++ src/practica5_TCP/ClientTCP.java
@@ -0,0 +1,24 @@
+package practica5_TCP;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientTCP {
+
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("127.0.0.1", 12000);
+
+        // Obtener flujos
+
+        // Leer de teclado
+
+        // mientras que lo leído sea correcto
+            // enviar mensaje
+            // recibir respuesta
+        
+        // Enviar FINISH
+        // recibir OK
+
+        // Finalizar y liberar recursos
+    }
+}
